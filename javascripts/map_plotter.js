43d8e168fb586e9484b2b8e61e9ed1e82185f374
Buddy Magsipoc <keikun17@gmail.com>
2014-12-30 20:43:41 +0800

    Site updated to fbf6705

--- javascripts/map_plotter.js
+++ javascripts/map_plotter.js
@@ -14,17 +14,30 @@
     x = d3.scale.linear().range([0, width]);
     y = d3.scale.linear().range([height, 0]);
     color = d3.scale.category10();
+    window.color_mapping = {
+      "Piranha Games": '#000000',
+      "Steiner": '#000099',
+      "Davion": '#ffff66',
+      "Kurita": '#cc0000',
+      "Marik": '#990099',
+      "None": '#000000',
+      "Liao": '#003300',
+      "Rasalhague": '#a0a0a0',
+      "Ghost Bear": '#c0c0c0',
+      "Jade Falcon": '#00ff00',
+      "Wolf": '#994c00',
+      "Smoke Jaguar": "#ffcc99"
+    };
     xAxis = d3.svg.axis().scale(x).orient("bottom");
     yAxis = d3.svg.axis().scale(y).orient("left");
     svg = d3.select("body").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    return d3.json("javascripts/mapdata.json", function(error, data) {
-      var d, legend, old_data, owners, prop;
+    return d3.json("https://static.mwomercs.com/data/cw/mapdata.json", function(error, data) {
+      var d, legend, old_data, owner_ids, owner_names, owners, prop;
       delete data.generated;
       window._mapdata = data;
       for (prop in data) {
         if (!__hasProp.call(data, prop)) continue;
         if (data.hasOwnProperty(prop)) {
-          console.log('');
           data[prop].position.x = +data[prop].position.x;
           data[prop].position.y = +data[prop].position.y;
           data[prop].owner_id = +data[prop].owner.id;
@@ -33,6 +46,8 @@
       old_data = data;
       data = [];
       owners = [];
+      owner_ids = [];
+      owner_names = [];
       for (prop in old_data) {
         if (!__hasProp.call(old_data, prop)) continue;
         if (old_data.hasOwnProperty(prop)) {
@@ -47,34 +62,32 @@
             id: d.owner_id,
             name: d.owner_name
           });
+          owner_ids.push(d.owner_id);
+          owner_names.push(d.owner_name);
           data.push(d);
         }
       }
-      window.owners = owners;
-      console.log(data);
+      window.owner_ids = $.unique(owner_ids);
+      window.owner_names = $.unique(owner_names);
       x.domain(d3.extent(data, function(d) {
         return d.position.x;
       })).nice();
       y.domain(d3.extent(data, function(d) {
         return d.position.y;
       })).nice();
-      svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + height + ")").call(xAxis).append("text").attr("class", "label").attr("x", width).attr("y", -6).style("text-anchor", "end").text("X");
-      svg.append("g").attr("class", "y axis").call(yAxis).append("text").attr("class", "label").attr("transform", "rotate(-90)").attr("y", 6).attr("dy", ".71em").style("text-anchor", "end").text("Y");
       svg.selectAll(".dot").data(data).enter().append("circle").attr("class", "dot").attr("r", 1.5).attr("cx", function(d) {
-        console.log(d);
         return x(d.position.x);
       }).attr("cy", function(d) {
         return y(d.position.y);
       }).style("fill", function(d) {
-        return color(d.owner_id);
+        return color_mapping[d.owner_name];
       });
       svg.selectAll("text").data(data).enter().append("text").attr("x", function(d) {
-        console.log(d);
         return x(d.position.x);
       }).attr("y", function(d) {
         return y(d.position.y);
       }).style("fill", function(d) {
-        return color(d.owner_id);
+        return color_mapping[d.owner_name];
       }).text(function(d) {
         return d.name;
       });
